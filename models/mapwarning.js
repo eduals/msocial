6791c3d8c3e0d08f91715a05bc7ad4320ef9acc8
Phong Mai <phongmh307@gmail.com>
2014-09-17 12:46:50 +0700

    added getwarning and sentwarning

--- models/mapwarning.js
+++ models/mapwarning.js
@@ -1,10 +1,12 @@
 var Mongoose = require('../lib/database').Mongoose
 
+var User = require('./user')
+
 var MapWarningSchema = new Mongoose.Schema({
 	type: 				{ type: String, 	require: true },
 	location:			{ type: Array,		require: true,	default: [] },
 	memo:					{ type: String, 	require: false },
-	sentBy:				{ type: Mongoose.Schema.Types.ObjectId,	require: true, 	ref: 'User' },
+	sentBy:				{ type: Mongoose.Schema.Types.ObjectId, ref: 'User' },
 	sentAt:				{ type: Date,			require: true,	default: Date.now }
 })
 
